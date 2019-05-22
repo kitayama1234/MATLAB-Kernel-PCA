5276c1c394ea65974797af36f0c3c8c1efa664f5
kitayama1234 <masakinene@ae.auone-net.jp>
2019-05-22 12:51:16 +0900

    update demo.m

--- demo.m
+++ demo.m
@@ -10,6 +10,7 @@ figure
 hold on
 gscatter(X(:, 1), X(:, 2), Y)
 plot(Xtest(:, 1), Xtest(:, 2), 'LineStyle', 'none', 'Marker', '>')
+legend(["train data class1", "train data class2", "train data class3", "train data class4", "test data"])
 title('original data')
 
 
@@ -26,13 +27,13 @@ kpca.kernel_params.gamma = 0.0001;
 % fit pca and get the coefficient for projection with dataset 'X'
 fit_pca(kpca, X, "gaussian");
 
-% set the subspace dimention M of projected data (M <= D, where D is the
-% dimention of the original data),
-% and project the data 'X' into the subspace by using the coefficient
+% set the subspace dimention number M of projected data 
+% (M <= D, where D is the dimention of the original data),
+% and project the train data 'X' into the subspace by using the coefficient
 M = 2;
 projected_X = project(kpca, X, M);
 
-% project the data 'Xtest' as well
+% project the test data 'Xtest' as well
 projected_Xtest = project(kpca, Xtest, M);
 
 
@@ -44,4 +45,5 @@ plot(projected_Xtest(:, 1), projected_Xtest(:, 2), 'LineStyle', 'none', 'Marker'
 title('pca with gaussian kernel')
 xlabel('principal dim')
 ylabel('second dim')
+legend(["train data class1", "train data class2", "train data class3", "train data class4", "test data"])
 
